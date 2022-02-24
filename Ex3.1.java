8ebd61c883c0e9006a7804f977706f80ad14412a
Igs <65882489+igsG@users.noreply.github.com>
2022-02-23 21:57:32 -0300

    Update Ex3.1.java

--- Ex3.1.java
+++ Ex3.1.java
@@ -1,8 +1,17 @@
-public class Ex1 {
+public class Ex3 {
 	public static void main(String[] args) {
-		System.out.println("EXPERIMENTANDO a Linguagem JaVa");
-		System.out.print("ADS - PUC Go");
-		System.out.println("***** ****");
-		System.out.print("ADS \n PUC Go");
-		}
+		System.out.println("     **");
+		System.out.println("    ****");
+		System.out.println("   ******");
+		System.out.println("  ********");
+		System.out.println(" **********");
+		System.out.println("************");
+		System.out.println(" **********");
+		System.out.println("  ********");
+		System.out.println("   ******");
+		System.out.println("    ****");
+		System.out.println("     **          Igor");
+		
+	}
+	
 }
