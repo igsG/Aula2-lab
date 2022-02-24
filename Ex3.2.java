2067cbacf2d76f883bd51a3210baf4b9ac483230
Igs <65882489+igsG@users.noreply.github.com>
2022-02-23 21:56:13 -0300

    Create Ex3.2.java

---
+++ Ex3.2.java
@@ -0,0 +1,11 @@
+public class Ex3d{
+	public static void main(String[] args) {
+		System.out.println("******* STAR PLAY *******");
+		System.out.println("  1 - Play the game");
+		System.out.println("  2 - Restart last game");
+		System.out.println("  3 - Configure game");
+		System.out.println("  0 - Exit");
+		System.out.println("*************************");
+		System.out.println("OPTION -> ");
+	}
+}
