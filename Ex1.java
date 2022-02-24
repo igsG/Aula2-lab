2e6a49f0c146a0174abaee7af00cb39b757cd00f
Igs <65882489+igsG@users.noreply.github.com>
2022-02-23 21:57:59 -0300

    Create Ex1.java

---
+++ Ex1.java
@@ -0,0 +1,8 @@
+public class Ex1 {
+	public static void main(String[] args) {
+		System.out.println("EXPERIMENTANDO a Linguagem JaVa");
+		System.out.print("ADS - PUC Go");
+		System.out.println("***** ****");
+		System.out.print("ADS \n PUC Go");
+		}
+}
